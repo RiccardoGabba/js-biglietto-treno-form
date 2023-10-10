bf1d0db3879db9f7269d6153fd3cde68c533333e
filippo <riccardo.gabba02@gmail.com>
2023-10-10 16:16:20 +0200

    added alert in case of no written data

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const button  = document.querySelector(".btn-success");
 console.log(button);
 
 button.addEventListener('click', function(){
-    let nome = document.getElementById('nome').value;
-    let km = document.getElementById('km').value;
+    let nome = document.getElementById('nome') .value;
+    let km = document.getElementById('km') .value;
     let eta = document.getElementById('eta').value;
     //controlli che l'utente abbia inserito i dati corretti
+    if( nome === "" || eta === "" ){
+    alert("inserisci due valori validi !")
+}
+
     console.log(nome);
+    console.log(km);
     console.log(eta);
+    
 })
