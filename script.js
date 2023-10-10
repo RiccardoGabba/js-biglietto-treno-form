6811c2455d710dfb9c47f876dc595e1e30337ba5
filippo <riccardo.gabba02@gmail.com>
2023-10-10 16:02:00 +0200

    fixed the option value

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ console.log(button);
 
 button.addEventListener('click', function(){
     let nome = document.getElementById('nome').value;
-    let km = document.getElementById('km') .value;
-    let eta = document.getElementById('eta') .value;
+    let km = document.getElementById('km').value;
+    let eta = document.getElementById('eta').value;
     //controlli che l'utente abbia inserito i dati corretti
     console.log(nome);
     console.log(eta);
