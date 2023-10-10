6bd7256537a23f388b9427a9454371e3f59fcf58
filippo <riccardo.gabba02@gmail.com>
2023-10-10 15:40:52 +0200

    added .value to the function

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const button  = document.querySelector(".btn-success");
 console.log(button);
 
 button.addEventListener('click', function(){
-    let nome = document.getElementById('nome');
-    let km = document.getElementById('km');
-    let eta = document.getElementById('eta');
+    let nome = document.getElementById('nome').value;
+    let km = document.getElementById('km') .value;
+    let eta = document.getElementById('eta') .value;
     //controlli che l'utente abbia inserito i dati corretti
     console.log(nome);
     console.log(eta);
