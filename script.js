ff18e6da0ef589e64bfd001ffe786492b8077128
filippo <riccardo.gabba02@gmail.com>
2023-10-10 17:57:56 +0200

    solved the problem

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 const button  = document.querySelector(".btn-success");
 console.log(button);
 
+const saluto = document.getElementById('saluto');
+
 button.addEventListener('click', function(){
     let nome = document.getElementById('nome') .value;
     let km = document.getElementById('km') .value;
     let eta = document.getElementById('eta').value;
     //controlli che l'utente abbia inserito i dati corretti
+
     if( nome === "" || eta === "" ){
     alert("inserisci due valori validi !")
-}   else{
-    console.log(nome);
-    console.log(eta);
-    console.log(km);
-    
-    const saluto = dcoument.getElementById(' slulto' );
-    const msgContainer = saluto.querySelector(' p' );
-    const box = saluto.querySelector(' .box' );
-    
-
-    console.log(saluto);
-    console.log(msgContainer);
-    console.log(box);
 }
+    const twentyPC = 20/100;
+    const fortyPC = 40/100;
+    const costKm = 0.21;
+    const totalCost = costKm * km
+    let result; 
 
 
+    if (eta === 'minorenne') {
+        result = totalCost - (totalCost * twentyPC);
+        result.toFixed(2);
+        } else if (eta === 'over 65') {
+            result= totalCost - (totalCost * fortyPC);
+            result.toFixed(2);
+    } else {
+        console.log(totalCost)
+        result = totalCost;
+        result.toFixed(2);
+    }
 
-
-    console.log(nome);
-    console.log(km);
-    console.log(eta);
+    saluto.innerHTML = result.toFixed(2);
     
 })
+    
+    
+     
+
+    
