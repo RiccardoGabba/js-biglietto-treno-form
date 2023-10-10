3ae2bedc749059b87105c60ea43e03798510eb41
filippo <riccardo.gabba02@gmail.com>
2023-10-10 15:31:46 +0200

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+const button  = document.querySelector(".btn-success");
+console.log(button);
+
+button.addEventListener('click', function(){
+    let nome = document.getElementById('nome');
+    let km = document.getElementById('km');
+    let eta = document.getElementById('eta');
+    //controlli che l'utente abbia inserito i dati corretti
+    console.log(nome);
+    console.log(eta);
+})
